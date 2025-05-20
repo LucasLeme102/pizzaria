010f4c233230002ae7a4fecad2381b0549076281
lucasleme102@gmail.com <lucasleme@Mac-mini-de-Lucas.local>
2025-05-20 13:56:02 -0300

    feat: criando repsonsividade melhroada em dispositivos movéis

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.getElementById("menu-toggle");
+    const menuLinks = document.getElementById("menu-links");
+  
+    menuToggle.addEventListener("click", () => {
+      menuLinks.classList.toggle("show");
+    });
+  });
